be8a102a133b345a4d4e7f719faf3dd1527d7190
Sherif Lawal <lawalsheriffnifemi@gmail.com>
2026-01-06 10:02:50 +0100

    initial commit

---
+++ packages/db/src/index.ts
@@ -0,0 +1,6 @@
+import { env } from "@chi-and-rose/env/server";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+import * as schema from "./schema";
+
+export const db = drizzle(env.DATABASE_URL, { schema });
